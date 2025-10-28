2f7678f52d5f9b4909848a61e12600a654b6359f
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-10-28 16:41:55 -0300

    touch

---
+++ src/main/java/cl/tenpo/challenge/infrastructure/asdController.java
@@ -0,0 +1,13 @@
+package cl.tenpo.challenge.infrastructure;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class asdController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+}
